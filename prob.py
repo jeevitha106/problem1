397ca4db442e6692a16a998609fd3b14889f7623
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-02-19 09:12:21 +0530

    version-3

--- prob.py
+++ prob.py
@@ -37,9 +37,6 @@ while True:
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
 
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
 
